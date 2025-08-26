f8e6c1630dce657b687e17cb14fb71dbfa4bf7e0
aziznurulloh8910 <aziznurulloh8@gmail.com>
2025-08-26 11:32:24 +0700

    add: policy to lesson api

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,12 +6,14 @@ use App\Models\Bootcamp;
 use App\Models\Category;
 use App\Models\Course;
 use App\Models\CourseFaq;
+use App\Models\Lesson;
 use App\Models\Review;
 use App\Models\Section;
 use App\Policies\BootcampPolicy;
 use App\Policies\CategoryPolicy;
 use App\Policies\CourseFaqPolicy;
 use App\Policies\CoursePolicy;
+use App\Policies\LessonPolicy;
 use App\Policies\ReviewPolicy;
 use App\Policies\SectionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -25,6 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         Category::class => CategoryPolicy::class,
         CourseFaq::class => CourseFaqPolicy::class,
         Review::class => ReviewPolicy::class,
+        Lesson::class => LessonPolicy::class,
     ];
 
     public function boot(): void
