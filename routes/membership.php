6b1df8076bf67ae9c5493c9aca3d2e65392f6e13
Raihan Adam <hallwack.id@gmail.com>
2025-10-07 17:11:25 +0700

    feat: add member routes to membership API

--- routes/membership.php
+++ routes/membership.php
@@ -7,4 +7,7 @@ Route::prefix('membership')->name('membership.')->group(function () {
 
     Route::get('transaction/datalist', [TransactionController::class, 'datalist'])->name('transaction.datalist');
     Route::apiResource('transaction', TransactionController::class);
+
+    Route::get('member/datalist', [MemberController::class, 'datalist'])->name('member.datalist');
+    Route::apiResource('member', MemberController::class);
 });
