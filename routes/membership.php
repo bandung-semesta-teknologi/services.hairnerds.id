1e66f0bbdfa2215f3640ae51558923fd1b80e4fe
Raihan Adam <hallwack.id@gmail.com>
2025-10-09 17:07:45 +0700

    feat(membership): add authentication middleware to membership routes

--- routes/membership.php
+++ routes/membership.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\Membership\MemberController;
 use App\Http\Controllers\Api\Membership\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('membership')->name('membership.')->group(function () {
+Route::prefix('membership')->middleware('membership.auth')->name('membership.')->group(function () {
 
     Route::get('transaction/datalist', [TransactionController::class, 'datalist'])->name('transaction.datalist');
     Route::apiResource('transaction', TransactionController::class);
