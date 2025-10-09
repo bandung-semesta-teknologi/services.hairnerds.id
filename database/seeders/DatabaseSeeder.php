1147414ec25228f0bef8c5c55d037253931055e8
Raihan Adam <hallwack.id@gmail.com>
2025-10-09 17:07:55 +0700

    feat(seeder): add MembershipSerialSeeder for generating membership serials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->createCourses();
         $this->createBootcamps();
         $this->callRelatedSeeders();
+        $this->callMembershipSerialSeeder();
     }
 
     private function createBaseUsers(): void
@@ -171,6 +172,13 @@ class DatabaseSeeder extends Seeder
             AnswerBankSeeder::class,
             EnrollmentSeeder::class,
             ProgressSeeder::class,
+        ]);
+    }
+
+    private function callMembershipSerialSeeder(): void
+    {
+        $this->call([
+            MembershipSerialSeeder::class,
         ]);
     }
 }
