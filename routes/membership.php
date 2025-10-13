dcd9418bbb8e8be7aa4831be8dbc9886e55ab021
Raihan Adam <hallwack.id@gmail.com>
2025-10-13 17:54:05 +0700

    feat(membership): add route for fetching latest membership transactions

--- routes/membership.php
+++ routes/membership.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('membership')->middleware('membership.auth')->name('membership.')->group(function () {
 
     Route::get('transaction/datalist', [TransactionController::class, 'datalist'])->name('transaction.datalist');
+    Route::get('transaction/latest', [TransactionController::class, 'latestTransaction'])->name('transaction.latest');
     Route::apiResource('transaction', TransactionController::class);
 
     Route::get('member/datalist', [MemberController::class, 'datalist'])->name('member.datalist');
