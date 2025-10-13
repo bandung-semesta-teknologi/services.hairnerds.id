e462afa136d1a78cfeafe7644ec58486b1ae1f3c
Raihan Adam <hallwack.id@gmail.com>
2025-10-13 08:21:48 +0700

    feat(membership): create TransactionStoreRequest for validating membership transaction data

---
+++ app/Http/Requests/Membership/TransactionStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Membership;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'card_number' => 'required|string|max:50',
+            'amount' => 'required|numeric|min:0',
+            'discount' => 'required|numeric|min:0',
+            'discount_type' => 'required|string|in:fixed,percentage',
+            'total' => 'required|numeric|min:0',
+            'merchant_id' => 'required|string|max:50',
+        ];
+    }
+}
