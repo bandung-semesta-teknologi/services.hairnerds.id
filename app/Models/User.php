035e53fa1f83119527b49b3dda6d4e63dac81076
Raihan Adam <hallwack.id@gmail.com>
2025-10-20 13:51:17 +0700

    fix(membership): add email_verified_at to fillable attributes in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'role',
+        'email_verified_at',
     ];
 
     protected $hidden = [
